72c53dc7c64b32c0ce2a82160c4418428b605c30
demsnvp7 <demsnvp7@M-GL0ZLX1.toronto.redknee.com>
2017-10-25 13:40:52 +0200

    request

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -2,6 +2,7 @@ package com.aws.codestar.projecttemplates.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Request;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,9 +12,9 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
 /**
  * Handler for requests to Lambda function.
  */
-public class HelloWorldHandler implements RequestHandler<Object, Object> {
+public class HelloWorldHandler implements RequestHandler<Request, Object> {
 
-    public Object handleRequest(final Object input, final Context context) {
+    public Object handleRequest(final Request input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         return new GatewayResponse("{ \"Output\": \"Hello World (bb)!\"}", headers, 200);
